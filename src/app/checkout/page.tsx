29271c0fb1816456377371617e79ab78154b05c4
Joseph Babu <josehp304@gmail.com>
2026-02-09 19:29:08 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,8 @@
-
 "use client";
 
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
